0fe840a84e3abbab258598659650b7f4cbeabbea
QuentinHbrt <quentin.hebert@hotmail.fr>
2021-03-07 16:57:43 +0100

    clean App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Heading } from '@theme-ui/components';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Heading>Dans ma Marmotte</Heading>
+    </Container>
   );
 }
 
