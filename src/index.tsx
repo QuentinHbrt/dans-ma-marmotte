aded7b929d3e32ea14d4a88d9fd41c32d73aa9d5
Guillaume Elloy <guillaume.elloy@gmail.com>
2021-03-08 15:47:12 +0100

    clean project

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "theme-ui";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { theme } from "./theme";
