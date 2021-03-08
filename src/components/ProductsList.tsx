951e238bb5b15af0e31679afda7c5d5bf1135115
QuentinHbrt <quentin.hebert@hotmail.fr>
2021-03-08 20:22:51 +0100

    fix: key item warning

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -11,7 +11,7 @@ type ProductsListProps = {
 export const ProductsList: FC<ProductsListProps> = (props) => {
     return (
         <ul>
-            {props.products.map((product) => <ProductItem product={product} />)}
+            {props.products.map((product) => <ProductItem key={product.id} product={product} />)}
         </ul>
     )
 }
