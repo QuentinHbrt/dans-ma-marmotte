405c7b926414779d97bb7020cb31f821307877a2
QuentinHbrt <quentin.hebert@hotmail.fr>
2021-03-09 14:39:45 +0100

    add Storage Form & Room Form

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import { Container, Heading } from '@theme-ui/components';
 import { ProductsList } from './components/ProductsList';
 import { PRODUCTS } from './api/data';
 import { ProductForm } from './components/ProductForm';
-import { Product } from './api/types';
+import { Product, Room, Storage } from './api/types';
+import { RoomForm } from './components/RoomForm';
+import { StorageForm } from './components/StorageForm';
 
 function App() {
 
   const [products, setProducts] = useState<Product[]>(PRODUCTS)
+  const [listOfRooms, setListOfRooms] = useState<Room[]>([])
+  const [storages, setStorages] = useState<Storage[]>([])
 
   function addProduct(newProduct: Product) {
     const newArrayOfProducts = [...products, newProduct]
@@ -15,11 +19,26 @@ function App() {
     // setProducts((currentProducts) => [...currentProducts, newProduct]);
   }
 
+  function addRoom(newRoom: Room) {
+    const newArrayOfRooms = [...listOfRooms, newRoom]
+    setListOfRooms(newArrayOfRooms);
+  }
+
+  function addStorage(newStorage: Storage) {
+    const newArrayOfStorages = [...storages, newStorage]
+    setStorages(newArrayOfStorages);
+  }
+
+  console.log('rooms', listOfRooms)
+  console.log('storages', storages)
+
   return (
     <Container>
       <Heading>{"Dans ma Marmotte"}</Heading>
       <ProductsList products={products} />
       <ProductForm onSubmitProduct={addProduct} />
+      <RoomForm onSubmitRoom={addRoom} />
+      <StorageForm onSubmitStorage={addStorage} roomsProperty={listOfRooms} />
     </Container>
   );
 }
---
+++ src/components/StorageForm.tsx
@@ -0,0 +1,52 @@
+import React, { ChangeEvent, FC, useState } from 'react';
+import { Input, Select } from 'theme-ui';
+import { Room, Storage } from '../api/types';
+
+type StorageFormProperties = {
+    readonly onSubmitStorage: (newStorage: Storage) => void
+    readonly roomsProperty: Room[]
+}
+
+export const StorageForm: FC<StorageFormProperties> = (properties) => {
+
+    const [storageName, setStorageName] = useState('')
+    const [selectedRoomId, setSelectedRoomId] = useState('')
+
+    function handleChangeStorageName(event: ChangeEvent<HTMLInputElement>) {
+        setStorageName(event.target.value)
+    }
+
+    function handleChangeStorageRoom(event: ChangeEvent<HTMLSelectElement>) {
+        setSelectedRoomId(event.target.value)
+    }
+
+    function handleSubmit(event: ChangeEvent<HTMLFormElement>) {
+        if (selectedRoomId === undefined) {
+            return
+        }
+        event.preventDefault();
+        const newStorage: Storage = {
+            id: Math.random().toString(),
+            name: storageName,
+            roomId: selectedRoomId
+        }
+        properties.onSubmitStorage(newStorage)
+        setStorageName('')
+        setSelectedRoomId('')
+
+    }
+
+    console.log(properties.roomsProperty)
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>STORAGE :</label>
+            <Input onChange={handleChangeStorageName} value={storageName} placeholder="name" type="text" name="name" id="name" />
+            <Select onChange={handleChangeStorageRoom} value={selectedRoomId} >
+                <option value={''}>{'Choisir...'}</option>
+                {properties.roomsProperty.map((room) => <option key={room.id} value={room.id}>{room.name}</option>)}
+            </Select>
+            <Input type="submit" value="envoyer" />
+        </form>
+    )
+}
