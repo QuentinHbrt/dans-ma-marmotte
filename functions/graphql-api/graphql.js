d87281e45c20c03bdd3820fc4f359da82511eed8
QuentinHbrt <quentin.hebert@hotmail.fr>
2021-03-18 15:04:40 +0100

    add graphql types

--- functions/graphql-api/graphql.js
+++ functions/graphql-api/graphql.js
@@ -2,16 +2,61 @@ const { ApolloServer, gql } = require('apollo-server-lambda');
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+  type Room {
+    id: ID!
+    name: String!
+    color: String!
+  }
+  
+  type Storage {
+    id: ID!
+    name: String!
+    roomId: ID!
+  }
+
+  type Product {
+    id: ID!
+    name: String!
+    storageId: ID!
+    category: String!
+  }
+
   type Query {
-    hello: String
+    rooms: [Room]
+    storages: [Storage]
+    products: [Product]
+  }
+
+  type Mutation {
+    addRoom(name: String!, color: String!, id: ID! ): Room
+    removeRoom(id: ID!): Room
   }
 `;
+
+
+let rooms = []
+// let storages = []
+// let products = []
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    rooms: () => rooms,
+    storages: () => ([{ id: 'storage-1', name: 'boite 1', roomId: 'room-1' }]),
+    products: () => ([{ id: 'product-1', name: 'draps', category: 'literie', storageId: 'storage-1' }]),
   },
+  Mutation: {
+    addRoom: (_, { name, id, color }) => {
+      const newRoom = { name, color, id: `room-${id}` }
+      rooms = [...rooms, newRoom]
+      return newRoom
+    },
+    removeRoom: (_, { id }) => {
+      const roomToRemove = rooms.find(room => room.id === id)
+      rooms = rooms.filter(room => room.id !== id)
+      return roomToRemove
+    }
+  }
 };
 
 const server = new ApolloServer({
