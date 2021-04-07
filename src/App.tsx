ea33a401d8f28732c9e09276acf04430ff6fdfa7
QuentinHbrt <quentin.hebert@hotmail.fr>
2021-04-07 13:32:14 +0200

    Add node sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.scss'
 import { Container, Heading } from '@theme-ui/components';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
