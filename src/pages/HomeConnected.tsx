3da29646cb1164276ea951209b63604d85cbb980
QuentinHbrt <quentin.hebert@hotmail.fr>
2021-04-08 16:23:32 +0200

    add HomeConnected

---
+++ src/pages/HomeConnected.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+export const HomeConnected: FC = () => {
+    return (
+        <main className='container-fluid'>
+            <section className="text-center">
+                <img src="images/IMG_4058.jpg" alt="amigos" />
+            </section>
+        </main>
+
+    );
+}
