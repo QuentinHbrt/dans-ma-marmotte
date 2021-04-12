1ca6c3f92e560dd00d75139c1f8e15d8a313fb76
QuentinHbrt <quentin.hebert@hotmail.fr>
2021-04-12 14:32:03 +0200

    delete icon menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,4 @@
 import React, { FC, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 export const NavBar: FC = (props) => {
@@ -12,7 +10,6 @@ export const NavBar: FC = (props) => {
         <div>
             <Navbar color="light" light>
                 <NavbarBrand href="/" className="mr-auto">{"Dans ma marmotte"}</NavbarBrand>
-                <i><FontAwesomeIcon icon={faBars} /></i>
                 <NavbarToggler onClick={toggleNavbar} className="mr-2" />
                 <Collapse isOpen={!collapsed} navbar>
                     <Nav navbar>
