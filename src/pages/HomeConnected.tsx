3be91c322a9927b93007220c22c6d5900b7f8aa9
QuentinHbrt <quentin.hebert@hotmail.fr>
2021-04-13 13:59:55 +0200

    update display HomeConnected

--- src/pages/HomeConnected.tsx
+++ src/pages/HomeConnected.tsx
@@ -2,15 +2,15 @@ import React, { FC } from 'react';
 
 export const HomeConnected: FC = () => {
     return (
-        <main className="masthead d-flex">
+        <section className="masthead d-flex">
             <div className="container text-center my-auto">
-                <h1 className="mb-1">Stylish Portfolio</h1>
+                <h1 className="mb-1">Inventaire</h1>
                 <h3 className="mb-5">
-                    <em>A Free Bootstrap Theme by Start Bootstrap</em>
+                    <em>Une facon simple d'organiser ses affaires</em>
                 </h3>
                 <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
             </div>
             <div className="overlay"></div>
-        </main>
+        </section>
     );
 }
