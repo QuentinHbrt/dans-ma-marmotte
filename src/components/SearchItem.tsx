722a095ea8d66ebe7b1958c251270b83a090b695
QuentinHbrt <quentin.hebert@hotmail.fr>
2021-05-05 14:11:06 +0200

    add SearchItem Element

---
+++ src/components/SearchItem.tsx
@@ -0,0 +1,37 @@
+import React, { FC, useState } from 'react';
+import { PRODUCTS } from '../api/data';
+import { Product } from '../api/types';
+
+type SearchItemProps = {
+    products: Product[]
+}
+
+export const SearchItem: FC = (props) => {
+
+    const [searchTerm, setSearchTerm] = useState('')
+    const BarStyling = { width: "20rem", background: "#F2F1F9", border: "none", padding: "0.5rem" };
+
+    return (
+        <div>
+            <input
+                style={BarStyling}
+                value={searchTerm}
+                placeholder={"search"}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            {/* {PRODUCTS.filter((value) => {
+                if (searchTerm === "") {
+                    return value
+                } else if (value.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+                    return value
+                }
+            }).map((value, key) => {
+                return (
+                    <div key={key}>
+                        <p>{value.name}</p>
+                    </div>
+                );
+            })} */}
+        </div>
+    );
+}
